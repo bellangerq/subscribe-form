adf996562e3f42bfbd9c1e8a705901dfe5e3095c
Quentin Bellanger <hello@quentin-bellanger.com>
2019-03-19 01:08:18 +0100

    [+] Init project

---
+++ index.js
@@ -0,0 +1,44 @@
+class SubscribeForm extends HTMLElement {
+  constructor() {
+    super()
+
+    // Retrieve attributes
+    this.name = this.getAttribute('name') || 'email'
+    this.label = this.getAttribute('label') || 'Email'
+    this.type = 'email'
+    this.placeholder = this.getAttribute('placeholder') || ''
+    this.required = this.getAttribute('required') || false
+
+    // Define element's markup
+    this.innerHTML = `
+      <form>
+        <label for="${this.name}">${this.label}</label>
+        <input type="${this.type}" required="${this.required}" id="${this.name}" placeholder="${this.placeholder}">
+        <input type="submit" value="Subscribe" />
+      </form>
+      <div role="alert" hidden class="success">
+        <p>You successfuly subscribed!</p>
+        <button role="button" aria-label="Close">
+          &times;
+        </button>
+      </div>
+    `
+
+    // Add events listeners
+    const form = this.querySelector('form')
+    const alert = this.querySelector("[role='alert']")
+    const alertButton = alert.querySelector("button")
+
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      alert.hidden = false
+    })
+
+    alertButton.addEventListener('click', () => {
+      alert.hidden = true
+    })
+  }
+}
+
+// Register the elements into `window`
+window.customElements.define('subscribe-form', SubscribeForm)
